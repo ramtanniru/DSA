1127019bc8eb7a7a18a58784b1f5a1a266d0e5e9
ram_tanniru <ramtanniru2@gmail.com>
2024-08-22 23:33:43 +0530

    496 Leetcode stack

---
+++ Python/Stack/nextGreaterElement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d = dict()
+        for i,x in enumerate(nums2):
+            d[x] = i
+        res = []
+        for i in nums1:
+            flag = True
+            for j in range(d[i]+1,len(nums2)):
+                if i<nums2[j]:
+                    res.append(nums2[j])
+                    flag = False
+                    break
+            if flag:
+                res.append(-1)
+        return res 
