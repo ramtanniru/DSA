cd8e1cb4a82cd1386651313037fb29ade83aa0f5
ram_tanniru <ramtanniru2@gmail.com>
2024-09-06 16:16:42 +0530

    DP

--- Python/DP/Algorithms/MCM.py
+++ Python/DP/Algorithms/MCM.py
@@ -1,5 +1,6 @@
 class Solution:
     def matrixMultiplication(self, N, arr):
+        # top-down memoisation
         def mcm(i,j):
             if i==j:
                 return 0
@@ -10,5 +11,12 @@ class Solution:
                 res = min(res,temp)
             dp[i][j] = res
             return dp[i][j]
-        dp = [[-1 for i in range(N)] for j in range(N)]
-        return mcm(1,N-1) 
+        dp = [[float('inf') for i in range(N)] for j in range(N)]
+        # tabulation
+        for i in range(len(dp)): dp[i][i] = 0
+        for i in range(len(dp)-1,0,-1):
+            for j in range(i+1,len(dp[0])):
+                for k in range(i,j):
+                    temp = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j]
+                    dp[i][j] = min(dp[i][j],temp)
+        return dp[1][N-1] 
