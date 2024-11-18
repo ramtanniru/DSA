4ba1cbd4457066a3797034a2cc8afb4de71d37da
ram_tanniru <ramtanniru2@gmail.com>
2024-11-18 15:06:38 +0530

    saddle point

--- Python/Companies/Eurofins/SaddlePoints.py
+++ Python/Companies/Eurofins/SaddlePoints.py
@@ -2,24 +2,24 @@ def saddlePoints(mat,n):
     r = set()
     c = set()
     for i in range(n):
-        miN = float('inf')
+        maX = float('-inf')
         rows = []
         for j in range(n):
-            if mat[i][j]<miN:
-                miN = mat[i][j]
-                rows = [(i,j)]
-            elif mat[i][j]==miN:
-                rows.append((i,j))
+            if mat[i][j]>maX:
+                maX = mat[i][j]
+                rows = [(i+1,j+1)]
+            elif mat[i][j]==maX:
+                rows.append((i+1,j+1))
         for node in rows:
             r.add(node)
-        maX = float('-inf')
+        miN = float('inf')
         cols = []
         for j in range(n):
-            if mat[j][i]>maX:
-                maX = mat[j][i]
-                cols = [(j,i)]
-            elif mat[j][i]==maX:
-                cols.append((j,i))
+            if mat[j][i]<miN:
+                miN = mat[j][i]
+                cols = [(j+1,i+1)]
+            elif mat[j][i]==miN:
+                cols.append((j+1,i+1))
         for node in cols:
             c.add(node)
     res = list(r.intersection(c))
